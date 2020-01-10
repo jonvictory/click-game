598e347b598831fdbee5415d64667766206d1a8d
jonhuynh <jonvictory@gmail.com>
2020-01-10 12:17:58 -0700

    figured out how to randomize my feed.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,15 +14,23 @@ class App extends React.Component {
   dealCards = () => {
     this.setState({ cards: imagemap });
   };
-
+  // shuffleCards = (array) => {
+  //   const cardArray = Object.keys(this.state.cards)
+  //   cardArray.sort(function(a,b){return 0.5 - Math.random()});
+    
+  // I can't figure out how to make this function work. But theoretically it should be possible to turn the cardArray.sort function into a function.}
+  
   render() {
+    const cardArray = Object.keys(this.state.cards)
+    
     return (
+      
       <div className="App">
         <Header />
         <div className="instruction no-marge no-pad">
           <div className="header-divs">
           <Cardtable dealCards={this.dealCards} />
-          {console.log(this)}
+          {/* {console.log(this)} */}
           </div>
           <div className="header-divs">
             <h3 className="no-marge title-center">
@@ -32,11 +40,15 @@ class App extends React.Component {
           <div className="header-divs"></div>
         </div>
         <div className="card-table no-marge no-pad">
-        {console.log(this)}
+        {/* {console.log(this)} */}
           <ul className="cards no-marge no-pad">
-            {Object.keys(this.state.cards).map(key => (
+            {cardArray.sort(function(a,b){return 0.5 - Math.random()}).map(key => (
               <Card key={key} details={this.state.cards[key]} />
+              
             ))}
+            {console.log(<Card/> + "card")}
+            {console.log(this.state.cards + "this.state.cards")}
+            
           </ul>
         </div>
         
